97388632f9d5c957b1e98975432b529a210ea634
Yiorgos Bosnakis <112989524+xyiorgyx@users.noreply.github.com>
2025-05-14 11:57:18 -0400

    Beginning code for project

---
+++ helpers/helpers.py
@@ -0,0 +1,8 @@
+def center_window(window, width=400, height=300):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x = int((screen_width / 2) - (width / 2))
+    y = int((screen_height / 2) - (height / 2))
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+from helpers.helpers import center_window
+
+root = tk.Tk()
+root.title("Firearms Manager")
+root.geometry(center_window(root, 500,500))
+
+
+
+
+
+root.mainloop()
